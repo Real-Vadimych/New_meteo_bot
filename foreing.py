1da77da533493007999a4f4401b589e129423aae
Vadik <vadim.kotelenets@gmail.com>
2022-03-06 16:30:06 +0300

    create params

--- foreing.py
+++ foreing.py
@@ -5,8 +5,16 @@ import requests
 
 def get_weather(req_type: str, airport: str) -> str:
 	# https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KDEN&hoursBeforeNow=1
-    url = f'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource={req_type}s&requestType=retrieve&format=xml&stationString={airport}&hoursBeforeNow=1'
-    xml = requests.get(url)
+    req_type += 's'
+    params = {
+        'dataSource': req_type,
+        'requestType': 'retrieve',
+        'format': 'xml',
+        'stationString': airport,
+        'hoursBeforeNow':1
+    }
+    url = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam'
+    xml = requests.get(url, params=params)
     soup = BeautifulSoup(xml.content, 'lxml')
 	# print(soup)
     try:
